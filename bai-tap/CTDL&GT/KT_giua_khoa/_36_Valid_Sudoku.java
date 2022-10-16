877cac554139bf91fbff61ca2d95638774620589
hoang180698 <hoang180698@gmail.com>
2022-10-17 00:44:22 +0700

    .

--- bai-tap/CTDL&GT/KT_giua_khoa/_36_Valid_Sudoku.java
+++ bai-tap/CTDL&GT/KT_giua_khoa/_36_Valid_Sudoku.java
@@ -2,7 +2,8 @@ import java.lang.reflect.Array;
 
 public class _36_Valid_Sudoku {
     public boolean isValidSudoku(char[][] board) {
-        
+        System.out.println();
         return false;
+        
     }
 }
